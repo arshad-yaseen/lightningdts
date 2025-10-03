50b9c8f89d15f0f366657f1917e3c0b31744a8ad
arshad <arshadpyaseen@gmail.com>
2025-10-03 19:12:48 +0530

    chore: oxc isolated decl ast

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, path::Path};
+
+use oxc_allocator::Allocator;
+use oxc_isolated_declarations::{IsolatedDeclarations, IsolatedDeclarationsOptions};
+use oxc_parser::Parser;
+use oxc_span::SourceType;
+
+fn main() -> std::io::Result<()> {
+    let name = env::args().nth(1).unwrap_or_else(|| "test.ts".to_string());
+    let path = Path::new(&name);
+    let source_text = std::fs::read_to_string(path)?;
+    let allocator = Allocator::default();
+    let source_type = SourceType::from_path(path).unwrap();
+
+    let ret = Parser::new(&allocator, &source_text, source_type).parse();
+
+    if !ret.errors.is_empty() {
+        for error in ret.errors {
+            let error = error.with_source_code(source_text.clone());
+            println!("{error:?}");
+        }
+        return Ok(());
+    }
+
+    println!("Original:\n");
+    println!("{source_text}\n");
+
+    let id_ret =
+        IsolatedDeclarations::new(&allocator, IsolatedDeclarationsOptions { strip_internal: true })
+            .build(&ret.program);
+
+    println!("Isolated Declarations AST:\n");
+    println!("{:#?}\n", id_ret.program);
+
+    if !id_ret.errors.is_empty() {
+        println!("Transformed dts failed:\n");
+        for error in id_ret.errors {
+            let error = error.with_source_code(source_text.clone());
+            println!("{error:?}");
+        }
+    }
+
+    Ok(())
 }
